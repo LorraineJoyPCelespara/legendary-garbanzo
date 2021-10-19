8b91a9b250e8f5cad23f26eb5c19aa3bbeea075b
LorraineJoyPCelespara <201980259@psu.palawan.edu.ph>
2021-10-19 19:23:48 +0800

    second commit

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,25 @@
 <?php
+session_start();
 
+$num_visits = $_SESSION['visit_count'];
+    if(isset($_GET['reset'])||!isset($num_visits)){
+        $num_visits = 0;
+    }
+    $num_visits++;
+    $_SESSION['visit_count'] = $num_visits;
 ?>
 
 <h1>Hello</h1>
 <p>You have visited this page <?= $num_visits ?> times</p>
-<p><a href="?reset=true">Reset</a></p>
+<p><a href = "?reset=true">Reset</a></p>
+
+<?php 
+    if($num_visits == 5){
+        echo "Thank you for visiting the page a lot.";
+    }
+
+    if($num_visits == 10){
+        header("Location: congratulations.php");
+        exit();
+    }
+?>
